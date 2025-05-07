ad85aacda9b5f31ea2b5bfc945a422304815698c
bakurkvaratskhelia <bakurikv@yahoo.com>
2025-05-07 11:28:21 +0300

    index hero bg fixed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM  from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { BrowserRouter } from "react-router-dom";
+import CheckoutPage from './pages/home/CheckoutPage.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -10,4 +11,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <App />
     </BrowserRouter>
   </React.StrictMode>
+);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <CheckoutPage />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
